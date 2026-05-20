73897959dd3d71863eb644486781f61b3ea0ffcf
Madhav Tibrewal <56196812+madhavtibrewal@users.noreply.github.com>
2026-05-20 18:57:17 -0400

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool dfs(vector<int>& nums, int i){
+        if(i == nums.size() - 1) return true;
+
+        int end = min((int)nums.size() - 1, i + nums[i]);
+
+        for(int j = i + 1; j <= end; j++){
+            if(dfs(nums, j)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+    bool canJump(vector<int>& nums) {
+        //return dfs(nums, 0);
+        int n = nums.size();
+
+        /*vector<bool> dp(n, false);
+
+        dp[n - 1] = true;
+
+        for(int i = n - 2; i >= 0; i--){
+
+            int end = min(n - 1, i + nums[i]);
+
+            for(int j = i + 1; j <= end; j++){
+                if(dp[j]){
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[0];*/
+        int dest = n - 1;
+
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] + i >= dest){
+                dest = i;
+            }
+        }
+
+        return dest == 0;
+    }
+};
