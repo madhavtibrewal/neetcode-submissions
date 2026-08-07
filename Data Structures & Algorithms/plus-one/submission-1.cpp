91df6c8b09abf21d07f7fe68179be7f0bb301ea0
Madhav Tibrewal <56196812+madhavtibrewal@users.noreply.github.com>
2026-08-07 11:14:42 -0700

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int sum = 0;
+        int n = digits.size();
+        vector<int> ans(n);
+        int carry  = 1;
+        for(int i = n - 1; i >= 0; i--){
+            int sig = carry + digits[i];
+            carry = sig / 10 ;
+            int remainder = sig % 10;
+
+            ans[i] = remainder;
+            cout << remainder <<" ";
+        }
+
+        if(carry != 0){
+            ans.insert(ans.begin(), carry);
+        }
+
+        return ans;
+
+        
+    }
+};
