f6a96e28ee2d00eb1d5df6b48c04cd54f35f2808
Madhav Tibrewal <56196812+madhavtibrewal@users.noreply.github.com>
2026-08-07 16:25:19 -0700

    Add: spiral-matrix - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int left = 0, right = n - 1;
+        int top = 0, bottom = m - 1;
+
+        while(left <= right && top <= bottom){
+            for(int i = left; i <= right; i++){
+                res.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i = top; i <= bottom; i++){
+                res.push_back(matrix[i][right]);
+            }
+            right--;
+            if(!(left <= right && top <= bottom)){
+                break;
+            }
+            for(int i = right; i >= left; i--){
+                res.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            for(int i = bottom; i >= top; i--){
+                res.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+
+        return res;
+    }
+};
