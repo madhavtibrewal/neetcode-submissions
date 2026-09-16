6300940cd3804c1c73edcec63ba3e9f7afc96186
Madhav Tibrewal <56196812+madhavtibrewal@users.noreply.github.com>
2026-09-17 04:58:19 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+        for(int b : bills){
+            if(b == 5){
+                five++;
+            }
+            else if(b == 10){
+                ten++;
+                five--;
+            }else if(b == 20){
+                if(ten > 0){
+                    ten--;
+                    five--;
+                }
+                else{
+                    five -= 3;
+                }
+            }
+
+            if(five < 0) return false;
+        }
+
+        return true;
+    }
+};
