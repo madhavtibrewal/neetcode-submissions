ce2021731d0f6f279b0b879aa17599d635be2210
Madhav Tibrewal <56196812+madhavtibrewal@users.noreply.github.com>
2026-09-18 03:51:28 +0530

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dp;
+    int rec(int n){
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = rec(n - 1) + rec(n - 2) + rec(n - 3);
+    }
+    int tribonacci(int n) {
+        dp.resize(n + 1, -1);
+        dp[n] = rec(n);
+        return dp[n];
+    }
+};
