be99e9ae5cc15fc2b070c138d412a5a6e48b46f2
Madhav Tibrewal <56196812+madhavtibrewal@users.noreply.github.com>
2026-09-18 03:56:57 +0530

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n <= 2) return 1;
+        int ans = 0;
+        int a = 0, b = 1, c = 1;
+
+        for(int i = 3; i <= n; i++){
+            ans = a + b + c;
+            a = b;
+            b = c;
+            c = ans;
+        }
+
+        return ans;
+    }
+};
