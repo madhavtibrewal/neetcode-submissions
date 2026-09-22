dc8007cc935ba912ac51eb725a1cf44d938438d3
Madhav Tibrewal <56196812+madhavtibrewal@users.noreply.github.com>
2026-09-22 05:31:04 +0530

    Add: candy - submission-0

---
+++ Data Structures & Algorithms/candy/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> cnt(n, 1);
+
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i - 1]){
+                cnt[i] = cnt[i - 1] + 1;
+            }
+        }
+        
+        for(int i = n - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                cnt[i] = max(cnt[i], cnt[i + 1] + 1);
+            }
+        }
+
+
+        return accumulate(cnt.begin(), cnt.end(), 0);
+    }
+};
